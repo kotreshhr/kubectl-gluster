114d709dd6c3f6bffcef7c179bd14a44a74ce0ff
root <root@benki1.lab.eng.blr.redhat.com>
2019-03-08 20:45:15 +0530

    Cleanup other clusterrole, clusterrolebindings and storageclasses
    Signed-off-by: KotreshHR <khiremat@redhat.com>

--- extra/cleanup.sh
+++ extra/cleanup.sh
@@ -1,3 +1,29 @@
 kubectl delete clusterroles etcd-operator
 kubectl delete clusterrolebindings etcd-operator
+
+kubectl delete clusterroles csi-nodeplugin
+kubectl delete clusterrolebindings csi-nodeplugin 
+
+kubectl delete clusterroles external-attacher-runner 
+kubectl delete clusterrolebindings csi-attacher-role 
+
+kubectl delete clusterroles external-provisioner-runner 
+kubectl delete clusterrolebindings csi-provisioner-role
+
+kubectl delete clusterroles kube-state-metrics
+kubectl delete clusterrolebindings kube-state-metrics 
+
+kubectl delete clusterroles node-exporter 
+kubectl delete clusterrolebindings node-exporter 
+
+kubectl delete clusterroles prometheus 
+kubectl delete clusterrolebindings prometheus 
+
+kubectl delete clusterroles prometheus-operator
+kubectl delete clusterrolebindings prometheus-operator 
+
+kubectl delete sc glusterfs-csi
+kubectl delete sc glustervirtblock-csi
+kubectl delete volumesnapshotclass glusterfs-csi-snap
+
 kubectl delete namespace gcs
